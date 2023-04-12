84c43a563b55a41f3e395ee0f818c542830f2f07
danj-replicated <danj@replicated.com>
2023-04-12 13:12:33 +0100

    support directories

--- scripts/rpl_preflight.sh
+++ scripts/rpl_preflight.sh
@@ -15,9 +15,19 @@ case "$1" in
     # if our chart location looks like a url, pull and extract it
     helm pull -d "${pull_dir}" --untar --untardir "${render_dir}" "$1"
     ;;
-  *)
-    # otherwise just try and extract it
+  *.tgz)
+    # if our chart location looks like a tgz, extract it
     tar -C "${render_dir}" -xvf "$1"
+    ;;
+  *)
+    # if not it's probably just a directory I guess?
+    if [[ -d "$1" ]]; then
+      cp "$1" "${render_dir}"
+    else
+      # exit if it's not though, we don't want to confuse helm
+      printf "%s\n" "I'm not sure what your chart repo actually is."
+      exit 1
+    fi
     ;;
 esac
 
